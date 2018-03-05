25f0929ed12c003f3b672cf5bcc922616b9a06a5
Carlos Guijuelo <Carlos Guijuelo@10.1.251.78>
2018-03-05 12:56:56 +0100

    Version 1

---
+++ src/example/MiClase.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package example;
+
+/**
+ *
+ * @author Carlos Guijuelo
+ */
+public class MiClase {
+    public int numero_mayor(int a, int b, int c) {
+        if (a > b && a > c) {
+            return a;
+        } else if (c > b) {
+            return c;
+        } else {
+            return b;
+        }
+    }
+}
